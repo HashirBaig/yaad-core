8405b477f111475cf1008809f2c98e55172f9e73
Hashir Baig <hashirbaig456@gmail.com>
2022-12-02 22:43:03 +0500

    Added pre-flight across the board

--- server.js
+++ server.js
@@ -11,8 +11,15 @@ const app = express()
 // Connect to DB
 connectDB()
 
-// Init middleware
+// Enable pre-flight across-the-board
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 app.use(cors())
+
+// Init middleware
 app.use(express.json())
 
 // Define routes
